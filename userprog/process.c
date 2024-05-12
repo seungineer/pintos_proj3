bf9ce879bb30ca6e213d35163706fddadc2f61ff
jh9193 <jh9193@naver.com>
2024-05-12 06:37:04 +0000

    feat: deny_write on excutable 구현

--- userprog/process.c
+++ userprog/process.c
@@ -504,6 +504,9 @@ static bool load(const char *file_name, struct intr_frame *if_) {
         }
     }
 
+    t->running = file;
+    file_deny_write(file);
+
     /* Set up stack. */
     if (!setup_stack(if_))
         goto done;
@@ -518,7 +521,7 @@ static bool load(const char *file_name, struct intr_frame *if_) {
 
 done:
     /* We arrive here whether the load is successful or not. */
-    file_close(file);
+    // file_close(file);
     return success;
 }
 
